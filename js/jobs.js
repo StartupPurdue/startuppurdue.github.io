13d0f2e011cce4fa3aef13515b0ffc76a84996f4
Anuraag Yachamaneni <ayachama@purdue.edu>
2018-12-17 18:23:25 -0500

    Finish up most of the jobs page

---
+++ js/jobs.js
@@ -0,0 +1,68 @@
+window.onload = () => {
+  const searchField = document.getElementById("search");
+  const jobContainer = document.getElementById("job-container");
+
+  let jobs = [
+    { 
+      company: "Mimir", 
+      company_description: "Mimir Classroom provides the tools for instructors to efficiently teach Computer Science courses of any scale without compromising quality of education for students.",
+      image: "https://pbs.twimg.com/profile_images/720662602599129088/F20WzdeB_400x400.jpg",
+      job_title: "Software Engineer Intern - W '19",
+      majors: "Computer Science, Computer Engineering, Electrical Engineering",
+      application_link: "http://google.com",
+      date: "December 2nd, 2018",
+      link: "mimir-job-posting"
+    },
+  ]
+
+  let jobElement = document.createElement("div");
+  let jobText = `
+        <div class="services job">
+          <span class="icon">
+            <img src="company-image" width="150px" height="150px">
+          </span>
+          <div class="desc">
+            <h3>company-name</h3>
+            <h4><strong>job-title</strong></h4>
+            <p class="job-descrip">majors</p>
+            <a class="more-info" href="jobs/job-link">Apply Now</a>
+            <p>posting-date</p>
+          </div>
+        </div>
+  `
+
+  const filterJob = (job, query) => {
+    return job.company.includes(query) || job.majors.includes(query) || job.job_title.includes(query);
+  }
+
+  const updateJobs = (jobs) => {
+    jobContainer.innerHTML = "";
+
+    jobs.forEach(job => {
+      jobElement = document.createElement("div");
+      jobElement.setAttribute("class", "col-md-4 text-center job-card");
+      jobElement.innerHTML = jobText;
+
+      jobElement.innerHTML = jobElement.innerHTML.replace("job-link", job.link);
+      jobElement.innerHTML = jobElement.innerHTML.replace("company-image", job.image);
+      jobElement.innerHTML = jobElement.innerHTML.replace("company-name", job.company);
+      jobElement.innerHTML = jobElement.innerHTML.replace("majors", job.majors);
+      jobElement.innerHTML = jobElement.innerHTML.replace("job-title", job.job_title);
+      jobElement.innerHTML = jobElement.innerHTML.replace("posting-date", job.date);
+
+      jobContainer.appendChild(jobElement);
+    })
+  }
+
+  searchField.addEventListener("keyup", (e) => {
+    jobContainer.innerHTML = "";
+
+    if(searchField.value != "") {
+      updateJobs(jobs.filter(job => filterJob(job, searchField.value)));
+    } else {
+      updateJobs(jobs);
+    }
+  })
+
+  updateJobs(jobs);
+}
